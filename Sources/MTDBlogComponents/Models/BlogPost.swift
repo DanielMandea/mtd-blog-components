33e993ba353e166804a927775eb2d921dee15a7f
Daniel Mandea <daniel.mandea@ro.ibm.com>
2019-09-28 10:56:27 +0200

    Done adding success for dinamic Blog Post

---
+++ Sources/MTDBlogComponents/Models/BlogPost.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Mandea on 05/09/2019.
+//
+
+import Foundation
+
+open class BlogPost: Codable {
+    
+    // MARK: - Public
+    
+    public var uniqueID: String
+    public var title: String?
+    public var subtitle: String?
+    public var thumbnailImageUrl: String?
+    public var created: Date
+    public var articles: [BlogArticle]?
+    
+    // MARK: - Init 
+    
+    public init(uniqueID: String = NSUUID().uuidString, title: String?, subtitle: String?, created: Date = Date()) {
+        self.uniqueID = uniqueID
+        self.title = title
+        self.subtitle = subtitle
+        self.created = created
+    }
+}
---
+++ Sources/MTDBlogComponents/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Daniel Mandea on 16/09/2019.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
